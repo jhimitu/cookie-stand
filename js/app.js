3553d71db2be58c138afd94925ca37b2a71a72dc
Jhia Turner <jhimitu@gmail.com>
2019-04-01 16:41:26 -0700

    add a method to generate a random number of customers to firstAndPike

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 let firstAndPike = {
@@ -5,68 +7,68 @@ let firstAndPike = {
   maxNumberOfCustomers: 65,
   averageNumOfCookiesPurchasedPerCustomer: 6.3,
   generateARandomNumberOfCustomers: function () {
+    console.log(Math.floor(Math.random() * (this.maxNumberOfCustomers - this.minNumberOfCustomers)) + this.minNumberOfCustomers);
+  },
+  // simulateCookiesPurchasedPerHour: function () {
 
-  } ,
-  simulateCookiesPurchasedPerHour: function () {
-
-  } ,
+  // },
   cookiePurchaseTotalsPerHour: [],
   totalCookiesPurchasedPerDay: 0
 };
 
-let seatacAirport = {
-  minNumberOfCustomers: 3,
-  maxNumberOfCustomers: 24,
-  averageNumOfCookiesPurchasedPerCustomer: 1.2,
-  generateARandomNumberOfCustomers: function () {
+// let seatacAirport = {
+//   minNumberOfCustomers: 3,
+//   maxNumberOfCustomers: 24,
+//   averageNumOfCookiesPurchasedPerCustomer: 1.2,
+//   generateARandomNumberOfCustomers: function () {
 
-  },
-  simulateCookiesPurchasedPerHour: function () {
+//   },
+//   simulateCookiesPurchasedPerHour: function () {
 
-  },
-  cookiePurchaseTotalsPerHour: [],
-  totalCookiesPurchasedPerDay: 0
-};
+//   },
+//   cookiePurchaseTotalsPerHour: [],
+//   totalCookiesPurchasedPerDay: 0
+// };
 
-let seattleCenter = {
-  minNumberOfCustomers: 11,
-  maxNumberOfCustomers: 38,
-  averageNumOfCookiesPurchasedPerCustomer: 2.3,
-  generateARandomNumberOfCustomers: function () {
+// let seattleCenter = {
+//   minNumberOfCustomers: 11,
+//   maxNumberOfCustomers: 38,
+//   averageNumOfCookiesPurchasedPerCustomer: 2.3,
+//   generateARandomNumberOfCustomers: function () {
 
-  },
-  simulateCookiesPurchasedPerHour: function () {
+//   },
+//   simulateCookiesPurchasedPerHour: function () {
 
-  },
-  cookiePurchaseTotalsPerHour: [] ,
-  totalCookiesPurchasedPerDay: 0  
-};
+//   },
+//   cookiePurchaseTotalsPerHour: [] ,
+//   totalCookiesPurchasedPerDay: 0  
+// };
 
-let capitolHill = {
-  minNumberOfCustomers: 20,
-  maxNumberOfCustomers: 38,
-  averageNumOfCookiesPurchasedPerCustomer: 2.3,
-  generateARandomNumberOfCustomers: function () {
+// let capitolHill = {
+//   minNumberOfCustomers: 20,
+//   maxNumberOfCustomers: 38,
+//   averageNumOfCookiesPurchasedPerCustomer: 2.3,
+//   generateARandomNumberOfCustomers: function () {
 
-  },
-  simulateCookiesPurchasedPerHour: function () {
+//   },
+//   simulateCookiesPurchasedPerHour: function () {
 
-  },
-  cookiePurchaseTotalsPerHour: [],
-  totalCookiesPurchasedPerDay: 0
-};
+//   },
+//   cookiePurchaseTotalsPerHour: [],
+//   totalCookiesPurchasedPerDay: 0
+// };
 
-let alki = {
-  minNumberOfCustomers: 2,
-  maxNumberOfCustomers: 16,
-  averageNumOfCookiesPurchasedPerCustomer: 4.6,
-  generateARandomNumberOfCustomers: function () {
+// let alki = {
+//   minNumberOfCustomers: 2,
+//   maxNumberOfCustomers: 16,
+//   averageNumOfCookiesPurchasedPerCustomer: 4.6,
+//   generateARandomNumberOfCustomers: function () {
 
-  },
-  simulateCookiesPurchasedPerHour: function () {
+//   },
+//   simulateCookiesPurchasedPerHour: function () {
 
-  },
-  cookiePurchaseTotalsPerHour: [],
-  totalCookiesPurchasedPerDay: 0
-};
+//   },
+//   cookiePurchaseTotalsPerHour: [],
+//   totalCookiesPurchasedPerDay: 0
+// };
 
